461ec87ff5d709ec8f41654965db1d3dc171c261
Fernanda Master <167934773+fernanda-master@users.noreply.github.com>
2025-07-08 11:40:34 -0300

    Delete N3 DEV site directory

--- N3 DEV site/tcc.js
+++
@@ -1,9 +0,0 @@
-// Selecionando o botão de toggle e o menu
-const navToggle = document.getElementById('nav-toggle');
-const navMenu = document.getElementById('nav-menu');
-
-// Adicionando o evento de clique para alternar a visibilidade do menu
-navToggle.addEventListener('click', () => {
-  // Alterna a classe 'show' para exibir ou esconder o menu
-  navMenu.classList.toggle('show');
-});
